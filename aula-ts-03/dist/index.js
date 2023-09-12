eb0640c9f5158b88cdcbc3701371c87e8d3e276a
vitor-f2f <vitor_henrique001@hotmail.com>
2023-09-12 13:41:31 -0300

    aula 3

---
+++ aula-ts-03/dist/index.js
@@ -0,0 +1,17 @@
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function sum(num1, num2) {
+    return num1 * num2;
+}
+function isEven(num) {
+    return num % 2 === 0;
+}
+function showResult(result) {
+    if (isEven(result)) {
+        console.log("The result is ".concat(result, " and it's even!"));
+    }
+    else {
+        console.log("The result is ".concat(result, " and it's even!"));
+    }
+}
